d18d7679a124146b86f3acf6ec0b5a50daa70f99
Ellen Dutra <36541266+ellendutra@users.noreply.github.com>
2024-05-27 12:23:23 -0300

    Atualiza About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export function About(){
         <p className="text-lg md:text-xl text-gray-800 p-4 md:mb-0 md:mr-4">Somos uma plataforma que oferece facilidade para que você possa se conectar com profissionais que estão dispostos a acolhê-la e orientá-la de forma gratuita. Através do agendamento de videochamadas, você poderá ter acesso a consultas com advogadas e psicólogas voluntárias de todo o país. Uma rede de acolhimento para pessoas gestantes, puérperas e mães solo que estão em situação de vulnerabilidade nos abrigos do RS, precisando urgentemente de suporte emocional e orientação jurídica.</p>
       </div>
       <div className="md:w-1/2 flex justify-center">
-        <Image alt="Logo da Amiga do RS" className="mx-auto" src="/images/logo.svg" width="170" height="300" />
+        <Image alt="Logo da Amiga do RS" className="mx-auto" src="./images/logo.svg" width="170" height="300" />
       </div>
     </div>
   )
